4d256ce143d7ee2231eaf23cbdaeafc46ee866da
Adnan Khalid Shah <adnankhalid3505@gmail.com>
2021-01-14 01:19:12 +0500

    Auto-generated commit

--- src/app/interfaces/artist.ts
+++
@@ -1,9 +0,0 @@
-export interface Artist {
-  id: string;
-  name: string;
-  image_url?: string,
-  thumb_url?: string,
-  facebook_page_url: string;
-  upcoming_event_count: number;
-  imageSrc?: string;
-}
